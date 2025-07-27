635495979a4b85b0e1a0ce171b6a6516c4cd6961
Chibueze <iwujichibueze7@gmail.com>
2025-07-27 12:23:05 +0100

    Redux toolkit

---
+++ features/pizza/pizzaSlice.js
@@ -0,0 +1,16 @@
+const createSlice = require("@reduxjs/toolkit").createSlice;
+const initialState = {
+  pizzaBase: 1000,
+};
+const pizzaSlice = createSlice({
+  name: "Pizza",
+  initialState,
+  reducers: {
+    pizza_order: (state) => {
+      state.pizzaBase--;
+    },
+  },
+});
+console.log(pizzaSlice);
+module.exports = pizzaSlice.reducer;
+module.exports.pizzaActions = pizzaSlice.actions;
