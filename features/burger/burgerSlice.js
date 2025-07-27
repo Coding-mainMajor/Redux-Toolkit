a65f517fac01fd542cfbe5074ca364e69a18d82b
Chibueze <iwujichibueze7@gmail.com>
2025-07-27 21:41:25 +0100

    second commit

--- features/burger/burgerSlice.js
+++ features/burger/burgerSlice.js
@@ -1,3 +1,5 @@
+const { pizzaActions } = require("../pizza/pizzaSlice");
+
 const createSlice = require("@reduxjs/toolkit").createSlice;
 const initialState = {
   burgerBuns: 200,
@@ -9,6 +11,11 @@ const burgerSlice = createSlice({
     burger_order: (state) => {
       state.burgerBuns--;
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(pizzaActions.pizza_order, (state) => {
+      state.burgerBuns--;
+    });
   },
 });
 
